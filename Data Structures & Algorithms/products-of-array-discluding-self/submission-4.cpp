9bdc8bfa0aa16026c7ee922072fc73d3bd1f64fc
Bogdan Putintsev <54261520+bogdanputintsev@users.noreply.github.com>
2026-08-13 16:10:37 +0200

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> suffix(nums.size());
+        suffix[0] = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            suffix[i] = suffix[i - 1] * nums[i - 1];
+        }
+
+        vector<int> postfix(nums.size());
+        postfix[nums.size() - 1] = 1;
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            postfix[i] = postfix[i + 1] * nums[i + 1];
+        }
+
+        vector<int> result(nums.size());
+        for (int i = 0; i < nums.size(); i++) {
+            result[i] = suffix[i] * postfix[i];
+        }
+
+        return result;
+    }
+};
+
+// 1 2 4 6
+
+// 1 1 2 8
