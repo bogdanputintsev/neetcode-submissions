b65246efd13b53344dc8a7e4395a34fbbd97e087
Bogdan Putintsev <54261520+bogdanputintsev@users.noreply.github.com>
2026-08-18 13:14:29 +0200

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int result = 0;
+        int maxRight = prices[prices.size() - 1];
+
+        for (int i = prices.size() - 2; i >= 0; i--) {
+            result = max(result, maxRight - prices[i]);
+            maxRight = max(maxRight, prices[i]);
+        }
+
+        return result;
+    }
+};
+
+// 4
+// 7,1,5,3,6,4
+//           .
