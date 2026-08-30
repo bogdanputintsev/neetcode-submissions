7bcc5b3c3ac4ecdd54b2b305651014ae92742013
Bogdan Putintsev <54261520+bogdanputintsev@users.noreply.github.com>
2026-08-30 11:33:02 +0200

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int upperBound(const vector<vector<int>>& matrix, int target) {
+        int lo = 0;
+        int hi = matrix.size();  // exclusive!
+
+        while (lo < hi) {
+            int mid = lo + ((hi - lo) / 2);
+
+            // note: for lowerBound it's >= 
+            
+            if (matrix[mid][0] > target) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = upperBound(matrix, target) - 1;
+        if (row < 0) {
+            return false;
+        }
+
+        int minColIdx = 0;
+        int maxColIdx = matrix[0].size() - 1;
+        while (minColIdx <= maxColIdx) {
+            const int colIdx = minColIdx + ((maxColIdx - minColIdx) / 2);
+            const int value = matrix[row][colIdx];
+            if (value == target) {
+                return true;
+            } else if (value > target) {
+                maxColIdx = colIdx - 1;
+            } else if (value < target) {
+                minColIdx = colIdx + 1;
+            }
+        }
+
+        return false;
+    }
+};
