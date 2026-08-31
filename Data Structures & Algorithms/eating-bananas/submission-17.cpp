ad616089f2b19430d628da2ba7c11bff6bc40262
Bogdan Putintsev <54261520+bogdanputintsev@users.noreply.github.com>
2026-08-31 10:09:06 +0200

    Add: eating-bananas - submission-17

---
+++ Data Structures & Algorithms/eating-bananas/submission-17.cpp
@@ -0,0 +1,39 @@
+
+
+class Solution {
+public:
+    int maxValue(const vector<int>& piles) {
+        int maxValue = 0;
+        for (int pile : piles) {
+            maxValue = max(maxValue, pile);
+        }
+
+        return maxValue;
+    }
+
+    int getHoursForAllPiles(const vector<int>& piles, int k) {
+        int sum = 0;
+        for (int pile : piles) {
+            sum += ceil(static_cast<double>(pile) / k);
+        }
+
+        return sum;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int lo = 1;
+        int hi = maxValue(piles);
+
+        while (lo < hi) {
+            int mid = lo + ((hi - lo) / 2);
+            int sum = getHoursForAllPiles(piles, mid);
+            if (sum <= h) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+};
