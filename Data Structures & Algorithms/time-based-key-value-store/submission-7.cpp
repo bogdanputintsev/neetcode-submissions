a5a6e0990af5c7ec1c659762d100d8b0d95e061c
Bogdan Putintsev <54261520+bogdanputintsev@users.noreply.github.com>
2026-09-03 10:29:42 +0200

    Add: time-based-key-value-store - submission-7

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-7.cpp
@@ -0,0 +1,49 @@
+class TimeMap {
+public:
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        hashmap[key].emplace_back(value, timestamp);
+    }
+    
+    string get(string key, int timestamp) {
+        auto it = hashmap.find(key);
+        if (it == hashmap.end()) {
+            return "";
+        }
+
+        vector<Node>& nodes { it->second };
+
+        int left = 0;
+        int right = nodes.size();
+
+        while (left < right) {
+
+            int mid = left + ((right - left) / 2);
+
+            if (nodes[mid].timestamp <= timestamp) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        if (left == 0) {
+            return "";
+        }
+
+        return nodes[left - 1].value;
+    }
+
+private:
+    static constexpr size_t MAX_POSSIBLE_TIMESTAMP = 2000000;
+
+    struct Node {
+        string value;
+        int timestamp;
+    };
+
+    unordered_map<string, vector<Node>> hashmap;
+};
